a605cd6be37743df9b1d62bab9d7ca349e8c4554
Brian.Schmitt <brianschmitt@gmail.com>
2014-10-16 14:15:40 -0400

    updated so script can be reran

--- setup.bat
+++ setup.bat
@@ -1,13 +1,19 @@
 cd %HOME%
 
-mklink .vimrc dotfiles\.vimrc
+dir .vimrc | find "<SYMLINK>"
+if %errorlevel% == 1 rm .vimrc -f && mklink .vimrc dotfiles\.vimrc
 
-mklink .ctags dotfiles\.ctags
+dir .ctags | find "<SYMLINK>"
+if %errorlevel% == 1 rm .ctags -f && mklink .ctags dotfiles\.ctags
 
-mklink .global_ignore dotfiles\.global_ignore
+dir .global_ignore | find "<SYMLINK>"
+if %errorlevel% == 1 rm .global_ignore -f && mklink .global_ignore dotfiles\.global_ignore
 
-mklink .gitconfig dotfiles\.gitconfig
+dir .gitconfig | find "<SYMLINK>"
+if %errorlevel% == 1 rm .gitconfig -f && mklink .gitconfig dotfiles\.gitconfig
 
-mklink .jshintrc dotfiles\.jshintrc
+dir .jshintrc | find "<SYMLINK>"
+if %errorlevel% == 1 rm .jshintrc -f && mklink .jshintrc dotfiles\.jshintrc
 
-mklink %UserProfile%\Documents\autohotkey.ahk %UserProfile%\dotfiles\autohotkey.ahk
+dir %UserProfile%\Documents\autohotkey.ahk | find "<SYMLINK>"
+if %errorlevel% == 1 rm %UserProfile%\Documents\autohotkey.ahk -f && mklink %UserProfile%\Documents\autohotkey.ahk %UserProfile%\dotfiles\autohotkey.ahk
