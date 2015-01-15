c05b9dface5117d610c8e48b72aca7bbe28dd96f
Brian Schmitt <brianschmitt@gmail.com>
2015-01-15 14:38:19 -0500

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -3,21 +3,23 @@
 # Ask for the administrator password upfront
 sudo -v
 
-
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-
-# Disable the �Are you sure you want to open this application?� dialog
+# Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Disable Resume system-wide
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 
 # I LOVE ME SOME speling autocorrect.
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool true
 
-
 # Finder: show hidden files by default
 defaults write com.apple.Finder AppleShowAllFiles -bool true
 
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 # Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
@@ -25,10 +27,36 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
+###############################################################################
+# SSD-specific tweaks #
+###############################################################################
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
+# Remove the sleep image file to save disk space
+sudo rm /Private/var/vm/sleepimage
+# Create a zero-byte file instead…
+sudo touch /Private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+sudo chflags uchg /Private/var/vm/sleepimage
+# Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-# Don�t display the annoying prompt when quitting iTerm
+# Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
