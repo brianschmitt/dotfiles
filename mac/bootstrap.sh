8d4aa09db8c44c47307e3083d94108172f3f7840
Brian.Schmitt <brianschmitt@gmail.com>
2015-01-16 08:25:58 -0500

    Updated script

--- mac/bootstrap.sh
+++ mac/bootstrap.sh
@@ -2,7 +2,7 @@
 
 cd ~
 git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
-./dotfiles/setup.sh
-./dotfiles/mac/brew.sh
-./dotfiles/mac/osx.sh
+. ~/dotfiles/setup.sh
+. ~/dotfiles/mac/brew.sh
+. ~/dotfiles/mac/osx.sh
 
