5fef5624783b68b00f98ca60b771a70cbbf0107e
Brian.Schmitt <brianschmitt@gmail.com>
2015-01-29 09:51:08 -0500

    Added keygen creation for new computer setup

---
+++ osx/gen_github_sshkey.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ssh-keygen -t rsa -C "brianschmitt@gmail.com" -f ~/.ssh/github_rsa
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/github_rsa
+
+pbcopy < ~/.ssh/github_rsa.pub
+
+read -p "Add key (in clipboard) to github and press enter to continue"
+
+ssh -T git@github.com
+
+read -p "Update remote on ~/dotfiles? [y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git remote set-url origin git@github.com:brianschmitt/dotfiles
+fi
