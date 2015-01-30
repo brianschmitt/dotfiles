184ef64947197914aa52f26afe20938f91f2b85d
Brian.Schmitt <brianschmitt@gmail.com>
2015-01-30 15:30:47 -0500

    Updated order

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd ~
-git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
-. ~/dotfiles/setup.sh
+
+. ~/dotfiles/set_symlinks.sh
 . ~/dotfiles/osx/brew.sh
 . ~/dotfiles/osx/osx.sh
 
+git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
+vim +:BundleInstall
