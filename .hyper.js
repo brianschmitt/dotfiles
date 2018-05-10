82a78473757c0b56196373b22ae6386237e8a23d
Brian Schmitt <brianschmitt@gmail.com>
2018-05-10 10:14:39 -0400

    Change quotes

--- .hyper.js
+++ .hyper.js
@@ -133,7 +133,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-star-wars'],
+  plugins: ["hyper-star-wars"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
