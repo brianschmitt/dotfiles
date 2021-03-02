cfe259f63c1ead742417c926893477de14f41f29
Brian Schmitt <bschmitt@gmail.com>
2021-03-02 09:09:00 -0500

    Updates

--- set_symlinks.sh
+++ set_symlinks.sh
@@ -1,27 +1,22 @@
 cd ~
 
-ln -sf dotfiles/.vimrc .vimrc
+ln -sf dotfiles/.aliases .aliases
 
 ln -sf dotfiles/.ctags .ctags
 
+ln -sf dotfiles/.exports .exports
+
 ln -sf dotfiles/.global_ignore .global_ignore
+
+ln -sf dotfiles/.vimrc .vimrc
 
 if [ "$(uname)" = "Darwin" ]; then
     ln -sf dotfiles/.gitconfig-osx .gitconfig
+    ln -sf dotfiles/.zshrc .zshrc
+
 elif [ "$(uname)" = "Linux" ]; then
     ln -sf dotfiles/.gitconfig-lnx .gitconfig
+    ln -sf dotfiles/.bashrc .bashrc
+    ln -sf dotfiles/.bash_profile .bash_profile
+    ln -sf dotfiles/.bash_prompt .bash_prompt
 fi
-
-ln -sf dotfiles/.jshintrc .jshintrc
-
-ln -sf dotfiles/.aliases .aliases
-
-ln -sf dotfiles/.bash_prompt .bash_prompt
-
-ln -sf dotfiles/.bash_profile .bash_profile
-
-ln -sf dotfiles/.bashrc .bashrc
-
-ln -sf dotfiles/.zshrc .zshrc
-
-ln -sf dotfiles/.exports .exports
