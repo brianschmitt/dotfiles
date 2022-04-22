6c227f6ace4f7150c89f2ff30b33ef3691465e99
Brian Schmitt <brianschmitt@gmail.com>
2022-04-22 14:03:50 -0400

    Check if HB is installed

--- osx/brew.sh
+++ osx/brew.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew Now"; \
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
 
 brew analytics off
 brew update
