c3aab6dbe7ca50d7bf8f24b029caaae6474559fc
Brian Schmitt <brianschmitt@gmail.com>
2023-09-21 13:25:37 -0400

    update npm/pip

--- win/update.ps1
+++ win/update.ps1
@@ -1,7 +1,9 @@
 winget upgrade --all
 
+npm update -g npm
 npm update -g # npm outdated -g
 
+python.exe -m pip install --upgrade pip
 pip freeze | ForEach-Object{$_.split('==')[0]} | ForEach-Object{pip install --upgrade $_}
 
 Update-Module
