c9faa3027b19ecc535663ba0e92ef5974f31d4ad
Brian Schmitt <brianschmitt@gmail.com>
2025-05-23 14:38:52 -0400

    Updates

--- win/winget_install.ps1
+++ win/winget_install.ps1
@@ -14,12 +14,12 @@ winget install -h -e --id Microsoft.PowerShell
 # Editors
 winget install -h -e --id vim.vim
 winget install -h -e --id Microsoft.VisualStudioCode
-winget install -h -e --id Microsoft.SQLServerManagementStudio
-winget install -h -e --id dbeaver.dbeaver
-winget install -h -e --id 3T.Robo3T
-winget install -h -e --id SlackTechnologies.Slack
-winget install -h -e --id Microsoft.Teams
-winget install -h -e --id Zoom.Zoom
+#winget install -h -e --id Microsoft.SQLServerManagementStudio
+#winget install -h -e --id dbeaver.dbeaver
+#winget install -h -e --id 3T.Robo3T
+#winget install -h -e --id SlackTechnologies.Slack
+#winget install -h -e --id Microsoft.Teams
+#winget install -h -e --id Zoom.Zoom
 
 # General Utils
 #winget install -e --id 7zip.7zip
@@ -29,16 +29,17 @@ winget install -h -e --id Ditto.Ditto
 winget install -h -e --id WinMerge.WinMerge
 winget install -h -e --id Obsidian.Obsidian
 winget install -h -e --id Microsoft.PowerToys
-winget install -h -e --id Logitech.Options
+#winget install -h -e --id Logitech.Options
 winget install -h -e --id BurntSushi.ripgrep.GNU
 winget install -h -e --id junegunn.fzf
-winget install -h -e --id Martchus.syncthingtray
+winget install -h -e --id Syncthing.Syncthing
 # Shell
 winget install -h -e --id Microsoft.WindowsTerminal
 #choco install sysinternals -y
 
 # Source Controls
 #winget install -h -e --id Git.Git
+winget install -h -e --id dandavison.delta
 #winget install -h -e --id GitExtensionsTeam.GitExtensions
 #choco install delta -y
 #winget install -h -e --id GitHub.cli
@@ -51,11 +52,11 @@ winget install -h -e --id Python.Python.3.10
 #choco install pip -y
 
 #.Net Utils
-winget install -h -e --id Microsoft.NuGet
-winget install -h -e --id icsharpcode.ILSpy
-winget install -h -e --id Microsoft.DotNet.SDK.6
-winget install -h -e --id Microsoft.DotNet.SDK.8
-winget install -h "NuGet Package Explorer"
+#winget install -h -e --id Microsoft.NuGet
+#winget install -h -e --id icsharpcode.ILSpy
+#winget install -h -e --id Microsoft.DotNet.SDK.6
+#winget install -h -e --id Microsoft.DotNet.SDK.8
+#winget install -h "NuGet Package Explorer"
 #choco install nuget.commandline -y
 #winget install -e --id Microsoft.AzureStorageExplorer
-winget install -h -e --id Microsoft.VisualStudio.2022.Professional
+#winget install -h -e --id Microsoft.VisualStudio.2022.Professional
